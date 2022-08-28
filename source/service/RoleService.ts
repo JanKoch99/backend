aa36731cdab9e7fc25906d8acd6aab93eefb303e
JanKoch99 <jankoch99@bluewin.ch>
2022-08-28 02:31:06 +0200

    heroku 2

--- source/service/RoleService.ts
+++ source/service/RoleService.ts
@@ -3,14 +3,14 @@ import {Role} from "../model/role.model";
 export class RoleService {
     public static async getRole(roleId: number) {
         return Role.findByPk(roleId)
-            .then(role => {
+            .then((role: any) => {
                 if (role) {
                     return Promise.resolve(role);
                 } else {
                     return Promise.reject("Person not found");
                 }
             })
-            .catch(e => {
+            .catch(() => {
                 return Promise.reject('Could not fetch person data')
             });
     }
